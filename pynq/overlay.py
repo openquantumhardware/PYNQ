openquantumhardware/PYNQ

commit b5881e0ea0a07e322dae29e135526a7f7e44fb8d

migrated pynq/metadata to pynq-metadata. conserved backwards compatability with __init__.py (#1591)

pynq/overlay.py
@@ -1,4 +1,5 @@
 #   Copyright (c) 2016-2021, Xilinx, Inc.
+#   Copyright (C) 2022 - 2026 Advanced Micro Devices, Inc.
 #   SPDX-License-Identifier: BSD-3-Clause
 
 import os
@@ -14,7 +15,7 @@ import pynqutils
 from pynqmetadata.frontends import Metadata
 
 from .bitstream import Bitstream
-from .metadata.append_drivers_pass import bind_drivers_to_metadata
+from pynqmetadata.views.runtime.append_drivers_pass import bind_drivers_to_metadata
 from .mmio import MMIO
 from .ps import Clocks
 from .registers import RegisterMap

pynq/metadata/__init__.py
@@ -1,0 +1,20 @@
+# Copyright (C) 2026 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: BSD-3-Clause
+
+from pynqmetadata.views.runtime.append_drivers_pass import (
+    DriverExtension,
+    bind_drivers_to_metadata,
+)
+from pynqmetadata.views.runtime.clock_dict_view import ClockDictView
+from pynqmetadata.views.runtime.gpio_dict_view import GpioDictView
+from pynqmetadata.views.runtime.hierarchy_dict_view import HierarchyDictView
+from pynqmetadata.views.runtime.interrupt_controllers_view import (
+    InterruptControllersView,
+)
+from pynqmetadata.views.runtime.interrupt_pins_view import InterruptPinsView
+from pynqmetadata.views.runtime.ip_dict_view import IpDictView
+from pynqmetadata.views.runtime.mem_dict_view import MemDictView
+from pynqmetadata.views.runtime.metadata_view import MetadataView
+from pynqmetadata.views.runtime.runtime_metadata_parser import (
+    RuntimeMetadataParser,
+)

pynq/metadata/append_drivers_pass.py
@@ -1,123 +1,5 @@
 # Copyright (C) 2022 Xilinx, Inc
+# Copyright (C) 2022 - 2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import warnings
-from typing import Dict
-
-from pydantic import Field
-from pynqmetadata import Core, MetadataExtension, Module, ProcSysCore, SubordinatePort
-from pynqmetadata.errors import UnexpectedMetadataObjectType
-
-DRIVERS_GROUP = "pynq.lib"
-
-
-class DriverExtension(MetadataExtension):
-    """Extends the metadata for an IP with driver
-    information"""
-
-    driver: object = Field(
-        ..., exclude=True, description="The runtime driver for this core"
-    )
-    device: object = Field(
-        ..., exclude=True, description="The device that this core has been loaded onto"
-    )
-
-
-def bind_driver(
-    port: SubordinatePort,
-    device: object,
-    ip_drivers: Dict[str, object],
-    default_ip: object,
-    ignore_version: bool = False,
-) -> None:
-    """Assigns a driver to this port"""
-    core = port.parent()
-    if isinstance(core, Core):
-        if core.vlnv.str in ip_drivers:
-            port.ext["driver"] = DriverExtension(
-                driver=ip_drivers[core.vlnv.str], device=device
-            )
-        else:
-            no_version_ip = core.vlnv.str.rpartition(":")[0]
-            if no_version_ip in ip_drivers:
-                if ignore_version:
-                    port.ext["driver"] = DriverExtension(
-                        driver=ip_drivers[no_version_ip], device=device
-                    )
-                else:
-                    other_versions = [
-                        v
-                        for v in ip_drivers.keys()
-                        if v.startswith(f"{no_version_ip}:")
-                    ]
-                    message = f"IP {core.ref} is of type {core.vlnv.str} a driver has been found for {other_versions}. Use ignore_version=True to use this driver."
-                    warnings.warn(message, UserWarning)
-                    port.ext["driver"] = DriverExtension(
-                        driver=default_ip, device=device
-                    )
-            else:
-                port.ext["driver"] = DriverExtension(driver=default_ip, device=device)
-    # else:
-    #    raise UnexpectedMetadataObjectType(
-    #        f"Trying to bind driver to {port.ref} but it has no parent. The parent is {core.ref} which has type {type(core)}"
-    #    )
-
-
-def bind_ps_driver(
-    core: ProcSysCore,
-    device: object,
-    ip_drivers: Dict[str, object],
-    default_ip: object,
-    ignore_version: bool = False,
-) -> None:
-    """Assigns a driver to the PS"""
-    if core.vlnv.str in ip_drivers:
-        core.ext["driver"] = DriverExtension(
-            driver=ip_drivers[core.vlnv.str], device=device
-        )
-    else:
-        no_version_ip = core.vlnv.str.rpartition(":")[0]
-        if no_version_ip in ip_drivers:
-            if ignore_version:
-                core.ext["driver"] = DriverExtension(
-                    driver=ip_drivers[core.vlnv.str], device=device
-                )
-            else:
-                other_versions = [
-                    v for v in ip_drivers.keys() if v.startswith(f"{no_version_ip}:")
-                ]
-                message = f"IP {core.ref} is of type {core.vlnv.str} a driver has been found for {other_versions}. Use ignore_version=True to use this driver."
-                warnings.warn(message, UserWarning)
-                core.ext["driver"] = DriverExtension(driver=default_ip, device=device)
-        else:
-            core.ext["driver"] = DriverExtension(driver=default_ip, device=device)
-
-
-def bind_drivers_to_metadata(
-    md: Module, device: object, ip_drivers: Dict[str, object], default_ip: object
-) -> Module:
-    """Passes over the metadata and for each subordinate port
-    extends the metadata to include drivers bound to it"""
-    for core in md.blocks.values():
-
-        if isinstance(core, Module):
-            bind_drivers_to_metadata(
-                md=core, device=device, ip_drivers=ip_drivers, default_ip=default_ip
-            )
-        for port in core.ports.values():
-            if isinstance(port, SubordinatePort):
-                bind_driver(
-                    port=port,
-                    device=device,
-                    ip_drivers=ip_drivers,
-                    default_ip=default_ip,
-                )
-        if isinstance(core, ProcSysCore):
-            bind_ps_driver(
-                core=core,
-                device=device,
-                ip_drivers=ip_drivers,
-                default_ip=default_ip,
-            )
-
-    return md
+from pynqmetadata.views.runtime.append_drivers_pass import *  # noqa: F403

pynq/metadata/clock_dict_view.py
@@ -1,70 +1,5 @@
 # Copyright (C) 2022 Xilinx, Inc
+# Copyright (C) 2022 - 2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import json
-from typing import Dict
-
-import pynqutils
-from pynqmetadata import Module, ProcSysCore, VersalProcSysCore
-from pynqmetadata.errors import FeatureNotYetImplemented
-
-
-def _default_repr(obj):
-    return repr(obj)
-
-
-class ClockDictView:
-    """
-    Provides a view onto the Metadata object that displays all
-    configurable clocks in the system. Models a dictionary, where
-    the key is the index for the clock and the values contain:
-        * 'enable' : int whether the clock is enabled
-        * 'divisor0' : int divisor value for the clock
-        * 'divisor1' : int divisor value for the clock
-    """
-
-    def __init__(self, module: Module) -> None:
-        self._md = module
-
-    @property
-    def clock_dict(self) -> Dict:
-        repr_dict = {}
-
-        for core in self._md.blocks.values():
-            if isinstance(core, VersalProcSysCore):
-                for i in range(4):
-                    repr_dict[i] = {}
-                    repr_dict[i]["enable"] = int(core.find_clock_enable(i))
-                    repr_dict[i]["frequency"] = core.find_clock_frequency(i)
-            elif isinstance(core, ProcSysCore):
-                for i in range(4):
-                    repr_dict[i] = {}
-                    repr_dict[i]["enable"] = int(core.find_clock_enable(i))
-                    for j in range(2):
-                        repr_dict[i][f"divisor{j}"] = core.find_clock_divisor(i, j)
-
-        return repr_dict
-
-    def items(self):
-        return self.clock_dict.items()
-
-    def __len__(self) -> int:
-        return len(self.clock_dict)
-
-    def __iter__(self):
-        for clock in self.clock_dict:
-            yield clock
-
-    def _repr_json_(self) -> Dict:
-        return json.loads(json.dumps(self.clock_dict, default=_default_repr))
-
-    def __getitem__(self, key: str) -> None:
-        return self.clock_dict[key]
-
-    def keys(self):
-        return self.clock_dict.keys()
-
-    def __setitem__(self, key: str, value: object) -> None:
-        """TODO: needs to send value into the model bypassing ip_dict
-        this will require view tranlation in the other direction"""
-        raise FeatureNotYetImplemented("IPDictView is currently only read only")
+from pynqmetadata.views.runtime.clock_dict_view import *  # noqa: F403

pynq/metadata/gpio_dict_view.py
@@ -1,52 +1,5 @@
 # Copyright (C) 2022 Xilinx, Inc
+# Copyright (C) 2022 - 2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import json
-from typing import Dict
-
-from pynqmetadata import Module, ProcSysCore, SubordinatePort
-from pynqmetadata.errors import FeatureNotYetImplemented
-
-from .append_drivers_pass import DriverExtension
-
-from .metadata_view import MetadataView
-
-
-class GpioDictView(MetadataView):
-    """
-    Provides a view onto the Metadata object that displays all
-    GPIO pins controlled by the PS. Models a dictionary where
-    the keys are the names of the pin and each entry contains:
-        * the index of the pin :int
-        * the current state of the pin : str
-    """
-
-    def __init__(self, module: Module) -> None:
-        super().__init__(module=module)
-        self._state = {}
-
-    @property
-    def view(self) -> Dict:
-        repr_dict = {}
-
-        for core in self._md.blocks.values():
-            if isinstance(core, ProcSysCore):
-                gpio = core.gpio
-                for n, i in gpio.items():
-                    repr_dict[n] = {}
-                    if n in self._state:
-                        repr_dict[n]["state"] = self._state[n]
-                    else:
-                        repr_dict[n]["state"] = None
-                    pins = set()
-                    for p in i["pins"]:
-                        ref: str = f"{p.parent().parent().hierarchy_name}/{p.name}"
-                        pins.add(ref)
-                    repr_dict[n]["pins"] = pins
-                    repr_dict[n]["index"] = int(i["index"])
-
-        return repr_dict
-
-    def __setitem__(self, key: str, value: object) -> None:
-        """Set the state of an item in the gpio_dict"""
-        self._state[key] = value
+from pynqmetadata.views.runtime.gpio_dict_view import *  # noqa: F403

pynq/metadata/hierarchy_dict_view.py
@@ -1,186 +1,5 @@
 # Copyright (C) 2022 Xilinx, Inc
+# Copyright (C) 2022 - 2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Dict, List
-from pydantic import Field
-
-import json
-from pynqmetadata import Module, ProcSysCore, SubordinatePort, Hierarchy
-
-from .ip_dict_view import IpDictView
-from .mem_dict_view import MemDictView
-from pynqmetadata import MetadataExtension
-
-from .metadata_view import MetadataView
-
-def _default_repr(obj):
-    return repr(obj)
-
-class HierarchyDriverExetension(MetadataExtension):
-    """Extends the metadata for hierarchies with PYNQ runtime driver information"""
-    driver:object = Field(..., exclude=True, description="the runtime driver for this hierarchy")
-    device:object = Field(..., exclude=True, description="the device this core has been loaded onto")
-    overlay:object = Field(..., exclude=True, description="the overlay that this driver is associated with")
-
-class HierarchyDictView(MetadataView):
-    """
-    Provides a hierarchy view onto the Metadata object that will display
-    all hierarchies of addressable IP from the Processing System.
-
-    All IP when parsed into the HWH file is flat with no hierarchies. 
-    However, there is an additional, full_name field that can be used to reconstruct
-    these hierarchies, which is what this view uses. 
-
-    This view models a dictionary where each key is the name of the hierarchy, and each
-    entry contains:
-        * a dictionary of all the IP contained within that level of the hierarchy.
-        * a dictionare of all the memory objects within that level of the hierarchy.
-        * a dictionary of sub-hierarchies contained within this level of the hierarchy.
-        * any drivers that have been assigned to this level of the hierarchy.
-    """
-
-    def __init__(self, 
-                module: Module, 
-                ip_view:IpDictView, 
-                mem_view:MemDictView, 
-                overlay: object,
-                hierarchy_drivers:object,
-                default_hierarchy:object,
-                device:object) -> None:
-        super().__init__(module=module)
-        self._ip_dict = ip_view
-        self._mem_dict = mem_view
-        self._hierarchy_drivers = hierarchy_drivers
-        self._default_hierarchy = default_hierarchy
-        self._device = device
-        self._overlay = overlay
-
-    def _hierarchy_walker(self, r:Dict, h:Hierarchy)->None:
-        """ recursive walk down the hierarchy h, adding IP
-        that is a match in ip_dict or mem_dict to the hierarchies
-        """
-        if h.name not in r:
-            r[h.name] = {}
-            r[h.name]["ip"] = {}
-            r[h.name]["memories"] = {}
-            r[h.name]["hierarchies"] = {}
-            r[h.name]["interrupts"] = {}
-            r[h.name]["gpio"] = {}
-            r[h.name]["fullpath"] = h.path
-            r[h.name]["md_ref"] = h
-
-        for ip in h._core_obj.values():
-            if ip.hierarchy_name in self._mem_dict:
-                name = ip.hierarchy_name.split("/")[-1]
-                r[h.name]["memories"][name] = self._mem_dict[ip.hierarchy_name]
-            else:
-                if ip.hierarchy_name in self._ip_dict: 
-                    name = ip.hierarchy_name.split("/")[-1]
-                    r[h.name]["ip"][name] = self._ip_dict[ip.hierarchy_name] 
-
-        for hier in h._hierarchies_obj.values():
-            self._hierarchy_walker(r[h.name]["hierarchies"], hier)
-
-    def _prune_unused_walker(self, r:Dict)->bool:
-        """ Walks down through the hierarchy dict, removing anything
-        that is empty """
-        del_list = []
-        for i,h in r["hierarchies"].items():
-            if self._prune_unused_walker(h):
-                del_list.append(i)
-
-        for i in del_list:
-            del r["hierarchies"][i]
-
-        if r["md_ref"].pr_region: # Do not prune empty PR regions
-            return False
-        else:
-            return len(r["ip"])==0 and len(r["memories"])==0 and len(r["hierarchies"])==0
-
-    def _replicate_subhierarchies(self, add_to_root:Dict, l:Dict)->None:
-        """ The original hierarchy dict includes the sub-hierarchies
-        of other hierarchies at the root of the hierarchy_dict.
-        This walks through and appends the sub-hierarchies to the root. 
-        """
-        for hname,h in l["hierarchies"].items():
-            add_to_root[h["fullpath"]] = h
-            self._replicate_subhierarchies(add_to_root=add_to_root, l=h)
-
-    def _assign_drivers(self, hier_dict:Dict)->None:
-        """Assigns drivers to the hierarchy if the pattern matches
-        in the driver class.
-        
-        Uses metadata extensions to append the driver information. First
-        checks to see that there is not already a driver assigned."""
-        for hier in hier_dict["hierarchies"].values():
-            self._assign_drivers(hier_dict=hier)
-
-        if "driver" not in hier_dict["md_ref"].ext:
-            driver = self._default_hierarchy
-            for hip in self._hierarchy_drivers:
-                if hip.checkhierarchy(hier_dict):
-                    driver = hip
-                    break #taken 
-            hier_dict["md_ref"].ext["driver"] = HierarchyDriverExetension(device=self._device, driver=driver, overlay=self._overlay)
-            hier_dict["device"] = self._device
-            hier_dict["driver"] = driver 
-            hier_dict["overlay"] = self._overlay 
-        else:
-            hier_dict["device"] = hier_dict["md_ref"].ext["driver"].device 
-            hier_dict["driver"] = hier_dict["md_ref"].ext["driver"].driver 
-            hier_dict["overlay"] = hier_dict["md_ref"].ext["driver"].overlay 
-
-    def _cleanup_metadata_hierarchy_references(self, hier_dict:Dict)->None:
-        """"Removes any reference to the metadata hierarchy objects from
-        the dictionary"""
-        if "md_ref" in hier_dict:
-            del hier_dict["md_ref"]
-        for hier in hier_dict["hierarchies"].values():
-            self._cleanup_metadata_hierarchy_references(hier)
-
-    @property
-    def view(self) -> Dict:
-        """
-        Walks down the hierarchy dict and whenever it encounters an IP
-        that is in the ip_dict or mem_dict keep it.
-        """
-        repr_dict = {}
-        top_level = self._md._hierarchies
-
-        # Build up the hierarchies
-        for hierarchy in top_level._hierarchies_obj.values():
-            self._hierarchy_walker(repr_dict, hierarchy)      
-
-        # Prune hierarchies that are not used
-        for item in repr_dict.values():
-            self._prune_unused_walker(item)
-
-        # Remove anything at the root that has nothing beneath it
-        del_list = []
-        for i_name, i in repr_dict.items():
-            if len(i["ip"])==0 and len(i["memories"])==0 and len(i["hierarchies"])==0:
-                if not i["md_ref"].pr_region:
-                    del_list.append(i_name)
-
-        # Remove everything flagged for removal
-        for d in del_list:
-            del repr_dict[d]
-
-        # the extra sub-hierarchies add them to the root
-        add_to_root = {}
-        for item in repr_dict.values():
-            self._replicate_subhierarchies(add_to_root, item)
-
-        for iname, i in add_to_root.items():
-            repr_dict[iname] = i
-
-        # Assign drivers to all the hierarchies (Writes into the central metadata with a metadata extension)
-        # If a driver is already associated with the hierarchy then grab that
-        for item in repr_dict.values(): 
-            self._assign_drivers(item)
-
-        # remove any references to the metadata hierarchies in the dict
-        for item in repr_dict.values():
-            self._cleanup_metadata_hierarchy_references(item)
-
-        return repr_dict
+from pynqmetadata.views.runtime.hierarchy_dict_view import *  # noqa: F403

pynq/metadata/interrupt_controllers_view.py
@@ -1,107 +1,5 @@
 # Copyright (C) 2022 Xilinx, Inc
+# Copyright (C) 2022 - 2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import json
-import re
-from typing import Dict, List
-
-from pydantic import Field
-from pynqmetadata import Core, MetadataExtension, Module, Signal
-
-from .metadata_view import MetadataView
-
-class InterruptControllerIndex(MetadataExtension):
-    index: int = Field(
-        ..., description="The index for an interrupt controller in the PYNQ runtime"
-    )
-
-
-
-class InterruptControllersView(MetadataView):
-    """
-    Provides a view onto the Metadata object that displays all the AXI interrupt
-    controllers that are accessible from the processing system. Models a dictionary
-    where the key is the name of the interrupt controller and each entry contains:
-        * parent: either another controller or the PS
-        * index: the index of this interrupt.
-
-    The PS is the root of this hierarchy and is unnamed.
-    """
-
-    def __init__(self, module: Module) -> None:
-        super().__init__(module=module)
-        self._state = {}
-        self._base_idx: int = 0
-
-    def _walk_for_irq_controllers(self, sig: Signal) -> List[Core]:
-        """From a PS IRQ pin, walk out from it and the concat blocks collecting all
-        the interrupt controller blocks"""
-        ret: List[Core] = []
-
-        # Get the index number from the port name (stf: I don't like this)
-        if sig.parent().parent().vlnv.name == "xlconcat":
-            idx_re = re.search("In([0-9]+)", sig.name)
-            if len(idx_re.groups()) == 1:
-                # idx = int(idx_re.group(1)) + base_idx
-                idx = self._base_idx
-            else:
-                raise ValueError(
-                    f"Trying to infer an interrupt index from the port name for {sig.ref} but it does not match the expected format signame={sig.name} groups={idx_re.groups()}"
-                )
-        else:
-            idx = self._base_idx
-
-        for dst in sig._connections.values():
-            if dst.parent().parent().vlnv.name == "axi_intc":
-                dst.parent().parent().ext[
-                    "interrupt_controller_index"
-                ] = InterruptControllerIndex(index=idx)
-                self._base_idx = self._base_idx + 1
-                ret.append(dst.parent().parent())
-            if dst.parent().parent().vlnv.name == "xlconcat":
-                concat = dst.parent().parent()
-                for port in concat.ports.values():
-                    if port.name != "dout":
-                        sig = port.sig()
-                        ret = ret + self._walk_for_irq_controllers(sig)
-        return ret
-
-    @property
-    def view(self) -> Dict:
-        repr_dict = {}
-        self._base_idx
-
-        ps_list = self._md.get_processing_systems()
-        if len(ps_list) > 1:
-            raise RuntimeError(
-                f"There is more than one processing system in the design, not sure how to proceed."
-            )
-        ps = ps_list[list(ps_list.keys())[0]]
-
-        ps_irq_signals = ps.get_irqs
-
-        # Find all the directly connected controllers to the ps
-        controller_list = []
-        for ps_irq in ps_irq_signals.values():
-            controller_list = controller_list + self._walk_for_irq_controllers(ps_irq)
-
-        for controller in controller_list:
-            repr_dict[controller.hierarchy_name] = {"name": controller.name}
-            repr_dict[controller.hierarchy_name]["parent"] = ""
-            if "interrupt_controller_index" in controller.ext:
-                repr_dict[controller.hierarchy_name]["index"] = controller.ext[
-                    "interrupt_controller_index"
-                ].index
-            else:
-                raise RuntimeError(
-                    f"Cannot determine the index for interrupt controller {controller.ref}"
-                )
-            repr_dict[controller.hierarchy_name]["raw_irq"] = ps.irq_map[
-                controller.ext["interrupt_controller_index"].index
-            ]
-
-        return repr_dict
-
-    def __setitem__(self, key: str, value: object) -> None:
-        """Set the state of an item in the interrupt_controllers"""
-        self._state[key] = value
+from pynqmetadata.views.runtime.interrupt_controllers_view import *  # noqa: F403

pynq/metadata/interrupt_pins_view.py
@@ -1,121 +1,5 @@
 # Copyright (C) 2022 Xilinx, Inc
+# Copyright (C) 2022 - 2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
-import json
-import re
-from typing import Dict, List
-
-from pydantic import Field
-from pynqmetadata import Core, MetadataExtension, Module, Signal
-from .metadata_view import MetadataView
-
-
-class InterruptIndex(MetadataExtension):
-    index: int = Field(
-        ...,
-        description="The interrupt index that this pin is associated with in the PYNQ runtime",
-    )
-    controller: str = Field(
-        default="unknown",
-        description="The name of the interrupt controller for this interrupt pin",
-    )
-
-
-def _default_repr(obj):
-    return repr(obj)
-
-
-class InterruptPinsView(MetadataView):
-    """
-    Provides a view onto the metadata object that displays all
-    the AXI interrupt controllers in the system that are 
-    attached to the Processing System interrupt line.
-
-    Models a dictionary where the Key is the name of the
-    interrupt pin and each entry contains:
-        * a reference to the parent controller : str
-        * an index : int
-    """
-
-    def __init__(self, module: Module, controllers) -> None:
-        super().__init__(module=module)
-        self._controllers = controllers
-        self._base_idx: int = 0
-
-        pins = []
-        for ctrler_name in self._controllers:
-            irq_controller = self._md.blocks[self._controllers[ctrler_name]["name"]]
-            if "intr" in irq_controller.ports:
-                pins = pins + self._walk_for_irq_pins(
-                    irq_controller.ports["intr"].sig()
-                )
-
-            for irq_pin in pins:
-                irq_pin.ext[
-                    "interrupt_index"
-                ].controller = irq_controller.hierarchy_name
-
-    def _walk_for_irq_pins(self, sig: Signal) -> List[Signal]:
-        """From and interrupt controller walk over connected concat blocks to
-        get all the pins that are connected"""
-        ret: List[Signal] = []
-
-        if sig.parent().parent().vlnv.name == "xlconcat":
-            idx_re = re.search("In([0-9]+)", sig.name)
-            if len(idx_re.groups()) == 1:
-                # idx = int(idx_re.group(1)) + self._base_idx
-                idx = self._base_idx
-            else:
-                raise ValueError(
-                    f"Trying to infer an interrupt index from the port name for {sig.ref} but it does not match the expected format signame={sig.name} groups={idx_re.groups()}"
-                )
-        else:
-            idx = self._base_idx
-
-        for dst in sig._connections.values():
-            if isinstance(dst.parent().parent(), Core):
-                if dst.parent().parent().vlnv.name == "xlconcat":
-                    concat = dst.parent().parent()
-                    for port in concat.ports.values():
-                        if port.name != "dout":
-                            sig = port.sig()
-                            ret = ret + self._walk_for_irq_pins(sig)
-                else:
-                    sig.ext["interrupt_index"] = InterruptIndex(index=idx)
-                    dst.ext["interrupt_index"] = InterruptIndex(index=idx)
-                    self._base_idx = self._base_idx + 1
-                    ret.append(dst)
-                    ret.append(sig)
-        return ret
-
-    @property
-    def view(self) -> Dict:
-        repr_dict = {}
-        self._base_idx = 0
-
-        for ctrler_name in self._controllers:
-            pins = []
-            irq_controller = self._md.blocks[self._controllers[ctrler_name]["name"]]
-            if "intr" in irq_controller.ports:
-                pins = pins + self._walk_for_irq_pins(
-                    irq_controller.ports["intr"].sig()
-                )
-
-            for irq_pin in pins:
-                full_path = f"{irq_pin.parent().parent().hierarchy_name}/{irq_pin.name}"
-                repr_dict[full_path] = {}
-                repr_dict[full_path]["controller"] = irq_controller.hierarchy_name
-                if "interrupt_index" in irq_pin.ext:
-                    repr_dict[full_path]["index"] = irq_pin.ext["interrupt_index"].index
-                    irq_pin.ext[
-                        "interrupt_index"
-                    ].controller = irq_controller.hierarchy_name
-                else:
-                    repr_dict[full_path]["index"] = 127
-                repr_dict[full_path]["fullpath"] = full_path
-
-        return repr_dict
-
-    def __setitem__(self, key: str, value: object) -> None:
-        """Set the state of an item in the interrupt_pins"""
-        self._state[key] = value
+from pynqmetadata.views.runtime.interrupt_pins_view import *  # noqa: F403

commit 3a8438990a4647ac917a03ac4058fd99689955fb

Fix KeyError 'divisor1' loading any overlay on Versal
Overlay.download() guards on "divisor0" being present in clock_dict and then
reads "divisor1" unconditionally. On Versal that raises, so BaseOverlay() --
any Overlay() -- fails outright:

    KeyError: 'divisor1'

Versal has a single divisor stage where Ultrascale has two, so
PYNQ-Metadata's ClockDictView emits enable/frequency/divisor0/src_sel for a
VersalProcSysCore and no divisor1. Clocks.set_pl_clk already takes
div1=None, so reading it with .get() is all that is needed.

This is a regression from b5881e0e, which replaced PYNQ's own
pynq/metadata/clock_dict_view.py with a shim onto PYNQ-Metadata's. The old
local view emitted only enable and frequency for Versal, so the "divisor0"
guard was False and the whole block was skipped; the pynq-metadata view adds
divisor0 without divisor1, so the guard now passes and the next line fails.

On Versal the call is a no-op either way: _ClocksVersal.set_pl_clk warns that
PL clocks are fixed by the boot PDI rather than setting anything.

Affects VCK190 as much as VRK160 -- no Versal board can load an overlay
without this.

pynq/overlay.py
@@ -424,7 +424,10 @@ class Overlay(Bitstream):
                 enable = self.clock_dict[i]["enable"]
                 if "divisor0" in self.clock_dict[i]:
                     div0 = self.clock_dict[i]["divisor0"]
-                    div1 = self.clock_dict[i]["divisor1"]
+                    # Versal has a single divisor stage, so PYNQ-Metadata's
+                    # clock_dict carries divisor0 but no divisor1 for a
+                    # VersalProcSysCore. set_pl_clk already accepts div1=None.
+                    div1 = self.clock_dict[i].get("divisor1")
                     if enable:
                         Clocks.set_pl_clk(i, div0, div1)
                     else: